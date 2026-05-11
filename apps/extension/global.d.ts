84392f3c5e11275327689f58d9cc46e4ac64af8a
Joaquin Terrasa <josocjoq+dev@pm.me>
2026-05-11 09:18:35 +0200

    fix(extension): resolve browser type redeclaration in global.d.ts

--- apps/extension/global.d.ts
+++ apps/extension/global.d.ts
@@ -2,6 +2,9 @@ import Browser from "webextension-polyfill";
 
 declare global {
   const browser: typeof Browser;
+}
+
+declare global {
   namespace browser {
     export * from "webextension-polyfill";
   }
