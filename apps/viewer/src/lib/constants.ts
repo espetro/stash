71dd933816d0c6562beaa2e7292726bbf108e7da
Joaquin Terrasa <josocjoq+dev@pm.me>
2026-05-28 23:35:26 +0200

    feat(viewer): add constants for browser extension install URLs

---
+++ apps/viewer/src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const INSTALL_FIREFOX_URL = `https://github.com/espetro/stash/blob/main/content/docs/getting-started.md`;
+export const INSTALL_CHROME_URL = `https://github.com/espetro/stash/blob/main/content/docs/getting-started.md`;
