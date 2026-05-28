0bf8251952449aeb67cfdccb533af802b50aaf83
Joaquin Terrasa <josocjoq+dev@pm.me>
2026-05-28 23:35:30 +0200

    refactor(viewer): format pages and update environment types

--- apps/viewer/src/env.d.ts
+++ apps/viewer/src/env.d.ts
@@ -3,6 +3,11 @@
 interface ImportMetaEnv {
   readonly SITE: string;
   readonly VITE_VIEWER_ORIGIN: string;
+  // * Used to connect to Posthog Analytics
   readonly VITE_PUBLIC_POSTHOG_KEY: string;
   readonly VITE_PUBLIC_POSTHOG_HOST?: string;
+  // * Used to link to project sites
+  readonly VITE_REPO_URL: string;
+  readonly VITE_CHROME_DOWNLOAD_URL: string;
+  readonly VITE_FIREFOX_DOWNLOAD_URL: string;
 }
